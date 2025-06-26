7aff7d9a1ca33aa38929e433f34b7bdf73872dd3
Elias <eliasformentini1@gmail.com>
2025-06-26 20:49:38 -0300

    backend-fix

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -6,9 +6,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const sequelize = new Sequelize(
-  process.env.appcontas,   // Nome do banco
-  process.env.root,   // Usuário do banco
-  process.env.contas,   // Senha do banco
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
     host: process.env.localhost, // Ex: 'localhost'
     dialect: 'mysql',
--- front/services/api.js
+++ front/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.x.x:3000', // seu IP local
+  baseURL: 'http://localhost:3000', // seu IP local
 });
 
 export default api;
+npx nodemon src/app.js
